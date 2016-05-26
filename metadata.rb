ffb6737a4785ceb77e9a5266cc1e933dc334d86e
MrMcCartney <reed.mccartney@cox.net>
2016-05-26 12:04:16 -0500

    Increment version number for new release.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'reed@hoegg.software'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures Mule ESB'
 long_description 'Installs/Configures Mule ESB'
-version          '0.1.1'
+version          '0.2.0'
 
 depends 'apt', '~> 3.0.0'
 depends 'java', '~> 1.39.0'
