3b16f44a7a4cc16f25ab60724e435d73911d612b
Ryan Hoegg <ryan.hoegg@gmail.com>
2016-05-27 14:58:41 -0700

    add test cookbook source and remove ignore for cookbooks:

---
+++ test/cookbooks/mule-test/metadata.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+name 'mule-test'
+maintainer 'Ryan Hoegg'
+maintainer_email 'ryan@hoegg.software'
+license 'Apache 2.0'
+description 'Mule ESB and API Gateway On Premise Wrapper Cookbook'
+
+version '0.1.0'
+
+supports 'ubuntu'
+
+depends 'aws' ,'~> 3.3.3'
+depends 'mule'
