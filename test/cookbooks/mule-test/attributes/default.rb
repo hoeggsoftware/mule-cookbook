6947e3c8b8260a4278eadb2e735fac4b3f380b93
MrMcCartney <reed.mccartney@cox.net>
2016-06-08 11:16:10 -0500

    Finished working provider for lwrp version of mule installation.

--- test/cookbooks/mule-test/attributes/default.rb
+++ test/cookbooks/mule-test/attributes/default.rb
@@ -1,3 +1,6 @@
-default[:java][:install_flavor] = "oracle"
-default[:java][:jdk_version] = "8"
-default[:java][:oracle][:accept_oracle_download_terms] = true
+default['java']['install_flavor'] = "oracle"
+default['java']['jdk_version'] = "8"
+default['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['mule-test']['user'] = 'mule'
+default['mule-test']['group'] = 'mule'
--- test/cookbooks/mule-test/recipes/default.rb
+++ test/cookbooks/mule-test/recipes/default.rb
@@ -8,4 +8,25 @@ aws_s3_file "/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip" do
     not_if do ::File.exists?('/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip') end
 end
 
-include_recipe 'mule::default'
+group node['mule-test']['group'] do
+end
+
+user node['mule-test']['user'] do
+    supports manage_home: true
+    shell '/bin/bash'
+    home "/home/#{node['mule-test']['user']}"
+    comment 'Mule user'
+    group node['mule-test']['group']
+end
+
+include_recipe 'java::default'
+
+mule_instance "mule-esb" do
+    version "3.8.0"
+    enterprise_edition true
+    home "/usr/local/mule-esb-test"
+    env "test"
+    user node['mule-test']['user']
+    group node['mule-test']['group']
+    action :create
+end
