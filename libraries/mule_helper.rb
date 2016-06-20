7ab817f070afc5b4a62695fc8725999c4d5d043f
MrMcCartney <reed.mccartney@cox.net>
2016-06-20 13:16:02 -0500

    Add amc_setup method to helper. Cleanup methods.

--- libraries/mule_helper.rb
+++ libraries/mule_helper.rb
@@ -3,7 +3,10 @@ require 'rest-client'
 module Mule
   module Helper
     def amc_setup(username, password, organization, environment)
-      registrationToken = ''
+      authToken = get_auth_token(username, password)
+      orgId = get_org_id(authToken, organization)
+      envId = get_env_id(authToken, orgId, environment)
+      registrationToken = get_arm_token(authToken, orgId, envId)
     end
 
     def get_auth_token(username, password)
@@ -11,9 +14,10 @@ module Mule
         { 'username' => username, 'password' => password }.to_json, 'Content-Type' => 'application/json')
       authToken = JSON.parse(response.body).values_at('access_token')[0]
     end
+    
     def get_org_id(auth, organization)
       response = RestClient.get('https://anypoint.mulesoft.com/accounts/api/profile',
-        'Authorization' => 'bearer ' + auth)
+        'Authorization' => "bearer #{auth}")
       orgList = JSON.parse(response.body).values_at('memberOfOrganizations')[0]
       orgId = ''
       orgList.each do |org|
@@ -24,9 +28,10 @@ module Mule
       end
       orgId
     end
+
     def get_env_id(auth, orgId, environment)
-      response = RestClient.get('https://anypoint.mulesoft.com/accounts/api/organizations/' + orgId,
-        'Authorization' => 'bearer ' + auth)
+      response = RestClient.get("https://anypoint.mulesoft.com/accounts/api/organizations/#{orgId}",
+        'Authorization' => "bearer #{auth}")
       envList = JSON.parse(response.body).values_at('environments')[0]
       envId = ''
       envList.each do |env|
@@ -37,9 +42,10 @@ module Mule
       end
       envId
     end
+
     def get_arm_token(auth, orgId, envId)
       response = RestClient.get('https://anypoint.mulesoft.com/hybrid/api/v1/servers/registrationToken',
-        'Authorization' => 'bearer ' + auth, 'X-ANYPNT-ORG-ID' => orgId, 'X-ANYPNT-ENV-ID'=> envId)
+        'Authorization' => "bearer #{auth}", 'X-ANYPNT-ORG-ID' => orgId, 'X-ANYPNT-ENV-ID'=> envId)
       regToken = JSON.parse(response.body).values_at('data')[0]
     end
   end
