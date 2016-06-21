c97e9587a3b05370a478352e2f1bcd4d92e9fbfc
MrMcCartney <reed.mccartney@cox.net>
2016-06-21 11:20:54 -0500

    Change formatting for consistency.

--- spec/unit/libraries/mule_helper_spec.rb
+++ spec/unit/libraries/mule_helper_spec.rb
@@ -1,31 +1,31 @@
 describe Mule::Helper do
-  let(:helper) { Class.new { include Mule::Helper}}
-  let(:username) {'testusername'}
-  let(:password) {'testpassword'}
-  let(:organization) {'testorg'}
-  let(:environment) {'testenv'}
-  let(:authToken) {'aa12345a-b123-1c2d-23ee-1234ff123gg4'}
-  let(:orgId) {'aaa12345-ab89-1234-cdef-08989cd0909e'}
-  let(:envId) {'abc1234a-bc56-de7f-12ab-123abc456def'}
-  let(:regToken) {'aa12345a-b123-1c2d-23ee-1234ff123gg4---12345'}
-  describe '.get_auth_token' do
-    it 'gets an authorization token' do
-      expect(helper.new.get_auth_token(username, password)).to eq authToken
+    let(:helper) { Class.new { include Mule::Helper}}
+    let(:username) {'testusername'}
+    let(:password) {'testpassword'}
+    let(:organization) {'testorg'}
+    let(:environment) {'testenv'}
+    let(:authToken) {'aa12345a-b123-1c2d-23ee-1234ff123gg4'}
+    let(:orgId) {'aaa12345-ab89-1234-cdef-08989cd0909e'}
+    let(:envId) {'abc1234a-bc56-de7f-12ab-123abc456def'}
+    let(:regToken) {'aa12345a-b123-1c2d-23ee-1234ff123gg4---12345'}
+    describe '.get_auth_token' do
+        it 'gets an authorization token' do
+            expect(helper.new.get_auth_token(username, password)).to eq authToken
+        end
     end
-  end
-  describe '.get_org_id' do
-    it 'gets an organization id' do
-      expect(helper.new.get_org_id(authToken, organization)).to eq orgId
+    describe '.get_org_id' do
+        it 'gets an organization id' do
+            expect(helper.new.get_org_id(authToken, organization)).to eq orgId
+        end
     end
-  end
-  describe '.get_env_id' do
-    it 'gets an environment id' do
-      expect(helper.new.get_env_id(authToken, orgId, environment)).to eq envId
+    describe '.get_env_id' do
+        it 'gets an environment id' do
+            expect(helper.new.get_env_id(authToken, orgId, environment)).to eq envId
+        end
     end
-  end
-  describe '.get_arm_token' do
-    it 'gets a registration token' do
-      expect(helper.new.get_arm_token(authToken, orgId, envId)).to eq regToken
+    describe '.get_arm_token' do
+        it 'gets a registration token' do
+            expect(helper.new.get_arm_token(authToken, orgId, envId)).to eq regToken
+        end
     end
-  end
 end
