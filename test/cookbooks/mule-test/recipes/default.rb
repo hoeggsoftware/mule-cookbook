745f18665de0de9b2d8e5da56e374252caa1cb9c
MrMcCartney <reed.mccartney@cox.net>
2016-08-17 11:51:24 -0500

    Reduce duplication for ubuntu and windows platforms and fix path names for ruby based actions.

--- test/cookbooks/mule-test/recipes/default.rb
+++ test/cookbooks/mule-test/recipes/default.rb
@@ -1,10 +1,11 @@
+mule_base = '/usr/local'
+temp = '/tmp/mule'
+jdk_install = 'jdk-8u91-linux-x64.tar.gz'
+mule_install = 'mule-ee-distribution-standalone-3.8.0.zip'
+
 if node['platform'] == 'ubuntu'
-    aws_s3_file '/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip' do
-        bucket 'hoegg-ci-data'
-        remote_path 'installs/mule-ee-distribution-standalone-3.8.0.zip'
-        aws_access_key node['aws']['access_key']
-        aws_secret_access_key node['aws']['secret']
-        not_if do ::File.exists?('/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip') end
+    directory temp do
+        recursive true
     end
 
     user node['mule-test']['user'] do
@@ -20,41 +21,18 @@ if node['platform'] == 'ubuntu'
 
     node.default['java']['install_flavor'] = 'oracle'
     node.default['java']['jdk_version'] = '8'
-    node.default['java']['oracle']['accept_oracle_download_terms'] = true
-    include_recipe 'java::default'
+    node.default['java']['jdk']['8']['x86_64']['url'] = "file:///#{temp}/#{jdk_install}"
+    node.default['java']['jdk']['8']['x86_64']['checksum'] = '6f9b516addfc22907787896517e400a62f35e0de4a7b4d864b26b61dbe1b7552'
 
-    mule_instance 'mule-esb' do
-        enterprise_edition true
-        home '/usr/local/mule-esb-test'
-        env 'test'
-        user node['mule-test']['user']
-        group node['mule-test']['group']
-        action :create
-    end
-
-    mule_instance 'mule-esb-2' do
-        enterprise_edition true
-        home '/usr/local/mule-esb-test-2'
-        env 'test'
-        user node['mule-test']['user']
-        group node['mule-test']['group']
-        action :create
-    end
 elsif node['platform'] == 'windows'
-    aws_s3_file ENV['TEMP'] + '\\mule-ee-distribution-standalone-3.8.0.zip' do
-        bucket 'hoegg-ci-data'
-        remote_path 'installs/mule-ee-distribution-standalone-3.8.0.zip'
-        aws_access_key node['aws']['access_key']
-        aws_secret_access_key node['aws']['secret']
-        not_if do ::File.exists?(ENV['TEMP'] + '\\mule-ee-distribution-standalone-3.8.0.zip') end
+    include_recipe 'windows::default'
+    mule_base = 'C:/Program\ Files/Mule'
+    temp = 'C:/tmp/mule'
+    jdk_install = 'jdk-8u91-windows-x64.exe'
+
+    directory temp do
+        recursive true
     end
-        aws_s3_file ENV['TEMP'] + '\\mule-ee-distribution-standalone-3.8.0.zip' do
-            bucket 'hoegg-ci-data'
-            remote_path 'installs/mule-ee-distribution-standalone-3.8.0.zip'
-            aws_access_key node['aws']['access_key']
-            aws_secret_access_key node['aws']['secret']
-            not_if do ::File.exists?(ENV['TEMP'] + '\\mule-ee-distribution-standalone-3.8.0.zip') end
-        end
 
     user node['mule-test']['user']
 
@@ -63,26 +41,44 @@ elsif node['platform'] == 'windows'
         members node['mule-test']['user']
     end
 
-    include_recipe 'windows::default'
     node.default['java']['install_flavor'] = 'windows'
     node.default['java']['jdk_version'] = '8'
-    include_recipe 'java::default'
+    node.default['java']['windows']['url'] = "file:///#{temp}/#{jdk_install}"
+    node.default['java']['windows']['package_name'] = 'Java(TM) SE Development Kit 8 (64-bit)'
+end
 
-    mule_instance 'mule-esb' do
-        enterprise_edition true
-        home 'c:\\Program Files\\Mule\\mule-esb-test'
-        env 'test'
-        user node['mule-test']['user']
-        group node['mule-test']['group']
-        action :create
-    end
+aws_s3_file "#{temp}/#{mule_install}" do
+    bucket 'hoegg-ci-data'
+    remote_path "installs/#{mule_install}"
+    aws_access_key node['aws']['access_key']
+    aws_secret_access_key node['aws']['secret']
+    not_if do ::File.exists?("#{temp}/#{mule_install}") end
+end
 
-    mule_instance 'mule-esb-2' do
-        enterprise_edition true
-        home 'c:\\Program Files\\Mule\\mule-esb-test-2'
-        env 'test'
-        user node['mule-test']['user']
-        group node['mule-test']['group']
-        action :create
-    end
+aws_s3_file "#{temp}/#{jdk_install}" do
+    bucket 'hoegg-ci-data'
+    remote_path "installs/#{jdk_install}"
+    aws_access_key node['aws']['access_key']
+    aws_secret_access_key node['aws']['secret']
+    not_if do ::File.exists?("#{temp}/#{jdk_install}") end
+end
+
+include_recipe 'java::default'
+
+mule_instance 'mule-esb' do
+    enterprise_edition true
+    home "#{mule_base}/mule-esb-test"
+    env 'test'
+    user node['mule-test']['user']
+    group node['mule-test']['group']
+    action :create
+end
+
+mule_instance 'mule-esb-2' do
+    enterprise_edition true
+    home "#{mule_base}/mule-esb-test-2"
+    env 'test'
+    user node['mule-test']['user']
+    group node['mule-test']['group']
+    action :create
 end
