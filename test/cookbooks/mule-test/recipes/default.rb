b7a13206af392a30e979e35b2eb685d536e79df3
Pierre Beucher <beucher.pierre@gmail.com>
2017-08-22 14:55:41 +0200

    Added mule-test attribute to run Kitchen tests with CE or EE

--- test/cookbooks/mule-test/recipes/default.rb
+++ test/cookbooks/mule-test/recipes/default.rb
@@ -1,11 +1,18 @@
 directory '/tmp/mule'
 
-aws_s3_file '/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip' do
-    bucket 'hoegg-ci-data'
-    remote_path 'installs/mule-ee-distribution-standalone-3.8.0.zip'
-    aws_access_key node['aws']['access_key']
-    aws_secret_access_key node['aws']['secret']
-    not_if do ::File.exists?('/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip') end
+if node['mule-test']['enterprise']
+  aws_s3_file '/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip' do
+      bucket 'hoegg-ci-data'
+      remote_path 'installs/mule-ee-distribution-standalone-3.8.0.zip'
+      aws_access_key node['aws']['access_key']
+      aws_secret_access_key node['aws']['secret']
+      not_if do ::File.exists?('/tmp/mule/mule-ee-distribution-standalone-3.8.0.zip') end
+  end
+else
+  remote_file "/tmp/mule/mule-standalone-3.8.0.zip" do
+    source 'https://repository-master.mulesoft.org/nexus/content/repositories/releases/org/mule/distributions/mule-standalone/3.8.0/mule-standalone-3.8.0.zip'
+    action :create
+  end
 end
 
 group node['mule-test']['group'] do
@@ -22,7 +29,7 @@ end
 include_recipe 'java::default'
 
 mule_instance 'mule-esb' do
-    enterprise_edition true
+    enterprise_edition node['mule-test']['enterprise']
     home '/usr/local/mule-esb-test'
     env 'test'
     user node['mule-test']['user']
@@ -31,7 +38,7 @@ mule_instance 'mule-esb' do
 end
 
 mule_instance 'mule-esb-2' do
-    enterprise_edition true
+    enterprise_edition node['mule-test']['enterprise']
     home '/usr/local/mule-esb-test-2'
     env 'test'
     user node['mule-test']['user']
